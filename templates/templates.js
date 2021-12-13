6a7417079addf1aa0252261563ef96ef9c3f27f8
Aaron Bassett <me@aaronbassett.com>
2021-12-13 10:17:05 -0500

    Use __dirname instead of cwd()

--- templates/templates.js
+++ templates/templates.js
@@ -2,13 +2,7 @@ const fs = require("fs")
 const path = require("path")
 
 const getTemplateString = (templateFileName) => {
-  const CWD = path.resolve(process.cwd())
-  console.log("CWD", CWD)
-  const TEMPLATE_PATH = path.resolve(
-    CWD,
-    "plugins/newrelic-netlify-build-plugin/templates"
-  )
-  const TEMPLATE_FILE_PATH = path.resolve(CWD, templateFileName)
+  const TEMPLATE_FILE_PATH = path.resolve(__dirname, templateFileName)
   console.log("TEMPLATE_FILE_PATH", TEMPLATE_FILE_PATH)
 
   const buffer = fs.readFileSync(TEMPLATE_FILE_PATH)
