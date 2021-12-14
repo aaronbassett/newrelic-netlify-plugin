345793eb6834261a0723153e4612ca30f39c6560
Aaron Bassett <me@aaronbassett.com>
2021-12-14 02:54:54 -0500

    Checking Markdown support in deployment summary

--- results.js
+++ results.js
@@ -35,49 +35,49 @@ class PluginResults {
       ].toString()
     ) {
       case [false, false, false].toString():
-        return "We have nothing to report"
+        return "_We have nothing to report_"
       case [true, false, false].toString():
         return "We marked this deployment"
       case [true, true, false].toString():
-        return `We marked this deployment and recorded ${pluralize(
+        return `We marked this deployment and recorded **${pluralize(
           "event",
           eventCount,
           true
-        )}`
+        )}**`
       case [true, true, true].toString():
-        return `We marked this deployment, recorded ${pluralize(
+        return `We marked this deployment, recorded **${pluralize(
           "event",
           eventCount,
           true
-        )}, and added browser monitoring to ${pluralize(
+        )}**, and added browser monitoring to **${pluralize(
           "page",
           htmlCount,
           true
-        )}`
+        )}**`
       case [true, false, true].toString():
-        return `We marked this deployment and added browser monitoring to ${pluralize(
+        return `We marked this deployment and added browser monitoring to **${pluralize(
           "page",
           htmlCount,
           true
-        )}`
+        )}**`
       case [false, true, false].toString():
-        return `We recorded ${pluralize("event", eventCount, true)}`
+        return `We recorded **${pluralize("event", eventCount, true)}**`
       case [false, true, true].toString():
-        return `We recorded ${pluralize(
+        return `We recorded **${pluralize(
           "event",
           eventCount,
           true
-        )} and added browser monitoring to ${pluralize(
+        )}** and added browser monitoring to **${pluralize(
           "page",
           htmlCount,
           true
-        )}`
+        )}**`
       case [false, false, true].toString():
-        return `We added browser monitoring to ${pluralize(
+        return `We added browser monitoring to **${pluralize(
           "page",
           htmlCount,
           true
-        )}`
+        )}**`
     }
   }
 
